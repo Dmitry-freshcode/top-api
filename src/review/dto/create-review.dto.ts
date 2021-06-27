349f7b66c90fe7b4ecb989800416b36b3f6b81ac
Dmitry Ivanov <securiti.zp.ua@gmail.com>
2021-06-27 21:50:06 +0300

    add test e2e

---
+++ src/review/dto/create-review.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateReviewDto{
+  @ApiProperty()
+  name: string;
+  @ApiProperty()
+  title: string;
+  @ApiProperty()
+  description: string;
+  @ApiProperty()
+  rating: number;
+  @ApiProperty()
+  productId: string;
+}
--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from '@typegoose/typegoose';
+import { Types } from 'mongoose';
 
 export interface ReviewModel extends Base {}
 export class ReviewModel extends TimeStamps {
@@ -11,6 +12,6 @@ export class ReviewModel extends TimeStamps {
   description: string;
   @prop()
   rating: number;
-  @prop({ type: () => Date })
-  createdAt: Date;
+  @prop()
+  productId: Types.ObjectId;
 }
