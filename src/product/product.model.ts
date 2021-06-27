f53b5c7aac6251a675473b9de3d922dc6163d97f
Dmitry Ivanov <securiti.zp.ua@gmail.com>
2021-06-27 17:03:33 +0300

    fix typegoose models

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,17 +1,37 @@
-export class ProductModel {
-  _id: string;
+import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose';
+
+class ProductCharacteristics{
+  @prop()
+  name: string;
+  @prop()
+  value: string;
+}
+
+export interface ProductModel extends Base {}
+export class ProductModel extends TimeStamps {
+  @prop()
   image: string;
+  @prop()
   title: string;
+  @prop()
   price: number;
+  @prop()
   oldPrice: number;
+  @prop()
   credit: number;
+  @prop()
   calculatedRating: number;
+  @prop()
   description: string;
+  @prop()
   advantages: string;
+  @prop()
   disAdvantages: string;
+  @prop({ type: () => [String] })
   category: string[];
+  @prop({ type: () => [String] })
   tags: string[];
-  characteristics: {
-    [key: string]: string;
-  };
+  @prop({ type: () => [ProductCharacteristics], _id: false })
+  characteristics: ProductCharacteristics[];
 }
