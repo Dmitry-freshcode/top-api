71c546b5c7a759d587cef986c157903e24d10eeb
Dmitry Ivanov <securiti.zp.ua@gmail.com>
2021-07-05 20:43:35 +0300

    complete topPage

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TopPageController } from './top-page.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { TopPageModel } from './top-page.model';
+import { TopPageService } from './top-page.service';
 
 @Module({
   controllers: [TopPageController],
+  providers: [TopPageService],
   imports: [
     TypegooseModule.forFeature([
       {
